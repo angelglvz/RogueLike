180cfbf917f05440be837af4d3a20982af4cf3a2
angelglvz <angelglvz99@gmail.com>
2026-09-16 16:38:18 +0200

    Commit Inicial

---
+++ RogueLikeV1/RogueLikeV1/Game.cs
@@ -0,0 +1,38 @@
+﻿using RLNET;
+using System;
+
+namespace RogueLikeV1
+{
+    public class Game
+    {
+        private static readonly int _screenWidth = 100;
+        private static readonly int _screenHeight = 70;
+
+        private static RLRootConsole _rootConsole;
+
+        public static void Main()
+        {
+            string fontFileName = @"Elementos\terminal8x8.png";
+            string consoleTitle = "RougeSharp V3 Tutorial - Level 1";
+
+            Console.WriteLine(fontFileName);
+            Console.WriteLine(System.IO.File.Exists(fontFileName));
+
+            _rootConsole = new RLRootConsole(fontFileName, _screenWidth, _screenHeight, 8, 8, 1f, consoleTitle);
+
+            _rootConsole.Update += OnRootConsoleUpdate;
+            _rootConsole.Render += OnRootConsoleRender;
+            _rootConsole.Run();
+        }
+
+        private static void OnRootConsoleUpdate(object sender, UpdateEventArgs e)
+        {
+            _rootConsole.Print(10, 10, "It worked!", RLColor.White);
+        }
+
+        private static void OnRootConsoleRender(object sender, UpdateEventArgs e)
+        {
+            _rootConsole.Draw();
+        }
+    }
+}
