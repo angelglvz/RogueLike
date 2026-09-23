8983088db23827ebb6ab916f8351b73e40cea854
angelglvz <angelglvz99@gmail.com>
2026-09-23 11:34:51 +0200

    Added simple attack mechanics

--- RogueLikeV1/Core/DungeonMap.cs
+++ RogueLikeV1/Core/DungeonMap.cs
@@ -1,6 +1,7 @@
 ﻿using RLNET;
 using RogueSharp;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace RogueLikeV1.Core
 {
@@ -148,6 +149,17 @@ namespace RogueLikeV1.Core
                 }
             }
             return false;
+        }
+
+        public void RemoveMonster(Monster monster)
+        {
+            _monsters.Remove(monster);
+            SetIsWalkable(monster.X, monster.Y, true);
+        }
+
+        public Monster GetMonsterAt(int x, int y)
+        {
+            return _monsters.FirstOrDefault(m => m.X == x && m.Y == y);
         }
     }
 }
